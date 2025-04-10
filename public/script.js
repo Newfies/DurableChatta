77959a06522dba6e8d19c6ed4aabf078870f9758
Newfie <157941859+Newfies@users.noreply.github.com>
2025-04-10 17:22:40 -0500

    d

--- public/script.js
+++ public/script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
             msg.remove();
         }
+    });
 
     msg.addEventListener('mouseenter', function(){
         canHideMSGs = true;
