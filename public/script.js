191113d108d65de32c6ca42c0150bc89213f83bc
Newfie <157941859+Newfies@users.noreply.github.com>
2025-04-10 17:05:15 -0500

    x

--- public/script.js
+++ public/script.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const msg = document.querySelector('.messages');
+    var canHideMSGs = false;
+
     document.addEventListener('keydown', (event) => {
         if (event.key === '~') {
             window.close();
         }
     });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace') {
+            if (canHideMSGs == true){
+                event.preventDefault();
+
+                msg.remove();
+            }
+        }
+    });
+
+    msg.addEventListener('mouseenter', function(){
+        canHideMSGs = true;
+    });
+
+    msg.addEventListener('mouseleave', function(){
+        canHideMSGs = false;
+    });
+
+
 });
