55ed53c96607fe918b9dc974ea44ab97078c1d48
Newfie <157941859+Newfies@users.noreply.github.com>
2025-04-10 17:11:32 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keydown', (event) => {
         if (event.key === '~') {
             window.close();
+            window.location("https://mail.google.com/mail/u/0/");
         }
     });
 
